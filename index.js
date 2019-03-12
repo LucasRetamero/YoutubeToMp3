f78f6f568ff76f0aff5bb74c3406a014e4608337
Lucas Retamero <lucasretamerogimenes@gmail.com>
2019-03-12 01:19:55 -0300

    more libraru and back-end

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //create vars
-var express = require('express');
-var app = express();
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
 
 //open pug file
 app.set('view engine','pug');
@@ -11,10 +12,19 @@ app.use(express.static('css'));
 app.use(express.static('js'));
 app.use(express.static('dist'));
 app.use(express.static('scss'));
+//use middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
 
 //create home route
 app.get('/',function(req,res){
 return res.render(__dirname+'/public/'+'home');
+});
+
+//create get date of form
+app.post('/download-video',function(req,res){
+//req.body = post, req.query = get
+return res.send(req.body);
 });
 
 //create listen event
