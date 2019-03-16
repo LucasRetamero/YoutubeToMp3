d5b7536b169863bc00d167dbf9769cfbf7707f51
Lucas Oliveira <lucasretamerogimenes@gmail.com>
2019-03-16 18:18:52 -0300

    library and functions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
+var horizon    = require('horizon-youtube-mp3');
 
 //open pug file
 app.set('view engine','pug');
@@ -25,6 +26,25 @@ return res.render(__dirname+'/public/'+'home');
 app.post('/download-video',function(req,res){
 //req.body = post, req.query = get
 return res.send(req.body);
+});
+
+
+horizon.getInfo('http://youtube.com/watch?v=NEA0BLnpOtg', function(err, e){
+ 
+  console.log(e);
+ 
+  /**
+     * Will Return:
+     *
+     * { isValid: true,
+     *   videoName: 'OZIELZINHO - TOP GEAR 2.0',
+     *   videoThumb: 'https://i.ytimg.com/vi/NEA0BLnpOtg/hqdefault.jpg?custom=true&w=320&h=180&stc=true&jpg444=true&jpgq=90&sp=68&sigh=FoGsoudXCGPU-Fb6epRh1eIzVDs',
+     *   videoTime: '2:35',
+     *   videoTimeSec 244,
+     *   videoThumbList: {sd: 'http...', mq: 'http...', hq: 'http...', hd: 'http...'},
+     *   videoFile: 'https://....'
+     *   videoFormats: [...]}
+     */
 });
 
 //create listen event
