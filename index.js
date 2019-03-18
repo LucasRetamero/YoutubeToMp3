0f0c4d4e1a032ad55ded4ea1d3845fc67cac64f9
Lucas Oliveira <lucasretamerogimenes@gmail.com>
2019-03-17 21:31:50 -0300

    work project

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //create vars
-var express    = require('express');
+var express      = require('express');
 var app          = express();
 var bodyParser   = require('body-parser');
 var horizon      = require('horizon-youtube-mp3');
 var path         = require('path');
 var downloadPath = path.join(__dirname);
-var testing      = "Hello World"; 
+
 //open pug file
 app.set('view engine','pug');
+
 //use foldes
 app.use(express.static('public'));
 app.use(express.static('bootstrap'));
@@ -15,6 +16,7 @@ app.use(express.static('css'));
 app.use(express.static('js'));
 app.use(express.static('dist'));
 app.use(express.static('scss'));
+
 //use middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
@@ -24,50 +26,33 @@ app.get('/',function(req,res){
 return res.render(__dirname+'/public/'+'home');
 });
 
-//create get date of form
-app.post('/download-video',function(req,res){
-//req.body = post, req.query = get
-return res.send(req.body);
-});
-
-//create success download route
-app.get('/download-success',function(req,res){
-return res.render(__dirname+'/public/'+'success');
-});
-
 //create download route
-app.get('/download-mp3',function(req,res){
+app.post('/download-mp3',function(req,res){
+if(req.body.urlVideo == "" || res.wayFolder == ""){ //case empty, no work will return to home 
+res.redirect('/');
+}else{ //case not empty, make the download and convertion to mp3
 horizon.downloadToLocal(
-  'https://www.youtube.com/watch?v=WcTRQXtXJPs',
-  downloadPath,
+  req.body.urlVideo,
+  req.body.wayFolder,
   null,
   null,
   null,
   onConvertVideoComplete,
   onConvertVideoProgress
 )
+}
 });
 
-//testing download of video
-/*
-horizon.downloadToLocal(
-  'https://www.youtube.com/watch?v=WcTRQXtXJPs',
-  downloadPath,
-  null,
-  null,
-  null,
-  onConvertVideoComplete,
-  onConvertVideoProgress
-);
- */
-function onConvertVideoComplete(err, result) {
+//Create to show the complete download
+function onConvertVideoComplete(err, result){
   console.log(err, result);
   // Will return...
   //null, conversionFileComplete
 }
- 
+
+//Create to show the progress of the donwloading 
 function onConvertVideoProgress(percent, timemark, targetSize) {
-  console.log('Progress:', percent, 'Timemark:', timemark, 'Target Size:', targetSize);
+  console.log('Progesso:',percent+'%');
   // Will return...
   // Progress: 90.45518257038955 Timemark: 00:02:20.04 Target Size: 2189
   // Progress: 93.73001672942894 Timemark: 00:02:25.11 Target Size: 2268
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 ﻿# Youtube to Mp3
 Projeto com objetivo unico e exclusivo de aprendizado,o Youtube contem em sua
-diretrizes,regras a qual eu não pretendo infringir.
+diretrizes regras, a qual eu não pretendo infringir.Curioso para saber como essas web application que faz a conversão do video do youtube para mp3 funciona,eu criei um para entender como funciona em seu back-end e com isso agregar mais conhecimento usando novas technologias.
+
+### Preview
+![Image of Yaktocat](https://github.com/LucasRetamero/YoutubeToMp3blob/public/img/project.png)
